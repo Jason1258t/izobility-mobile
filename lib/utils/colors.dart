b6214299537f4a34c418a8a00db2fc64444c0f4b
kharlamcik <kkkharlamcik@vk.com>
2023-09-04 21:57:21 +0700

    AppColors

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -1,3 +1,28 @@
-class AppColors {
+import 'package:flutter/material.dart';
 
+class AppColors{
+  static const pageBackground = Color(0xFFF4F4F4);
+  static const backgorundSecondary = Color(0xFFF2F2F2);
+  static const backgroundContent = Colors.white;
+  static const background = Color(0xFFEFEFF0);
+  static const textPrimaryBackground = Color(0xFFEFEFF0);
+  static const disable = Color(0xFFB0B0B0);
+  static const textPrimary = Colors.black;
+  static const textSecondary = Color(0xFF747474);
+  static const textTertiary = Color(0xFFAAAAAA);
+  static const textContrast = Colors.white;
+  static const textLink = Color(0xFF2D81E0);
+  static const cityText = Color(0xFF033E67);
+  static const primary = Color(0xFF1E45A2);
+  static const negative = Color(0xFFE64646);
+  static const positive = Color(0xFF2F8D32);
+  static const neutral = Color(0x7FB4B4B4);
+  static const adtimer = Color(0xFFFACC74);
+  static const add = Color(0xFFFFFCE5);
+  static const lotteryNeutral = Color(0xFFFFF9E6);
+  static const game = Color(0xFFFFE3CE);
+  static const city = Color(0xFFD0EBFE);
+  static const charity = Color(0xFFDF656D);
+  static const sale = Color(0xFFEB2121);
+  static const border = Color(0xFFD9D9D9);
 }
