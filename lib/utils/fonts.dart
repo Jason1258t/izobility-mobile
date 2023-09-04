e41fbbbf50fd47649e087060cf9c858e8988eeda
Jason1258t <ivandomogatskij@gmail.com>
2023-09-04 16:57:35 +0300

    k

--- lib/utils/fonts.dart
+++ lib/utils/fonts.dart
@@ -1,5 +1,10 @@
+import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class AppFonts {
-  static const _font = GoogleFonts.inter;
+  static final TextStyle _font = GoogleFonts.inter();
+
+  static final font16gray = _font.copyWith(
+
+  );
 }
