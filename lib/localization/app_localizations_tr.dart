baf5590c6cb9e0be5a10a5855d13b1669e3b3348
Jason1258t <ivandomogatskij@gmail.com>
2024-01-03 19:07:28 +0300

    ы

---
+++ lib/localization/app_localizations_tr.dart
@@ -0,0 +1,609 @@
+import 'app_localizations.dart';
+
+/// The translations for Turkish (`tr`).
+class AppLocalizationsTr extends AppLocalizations {
+  AppLocalizationsTr([String locale = 'tr']) : super(locale);
+
+  @override
+  String get objects => '- Nesnele';
+
+  @override
+  String get incorrect_code => '* Yanlış ko';
+
+  @override
+  String get incorrect_email => '* Posta yanlı';
+
+  @override
+  String get two_fa => '2FA İki Faktör Kimlik Doğrulamas';
+
+  @override
+  String get address => 'Adre';
+
+  @override
+  String get account => 'Hesa';
+
+  @override
+  String get activate => 'Etkinleştirme';
+
+  @override
+  String get activity => 'Aktivit';
+
+  @override
+  String get promotion => 'Terf';
+
+  @override
+  String get without_phone_not_more_functions => 'Telefon numarasını onaylamadan, birçok uygulamaya erişiminiz olmayaca';
+
+  @override
+  String get safety => 'Emniye';
+
+  @override
+  String get exchange => 'Değişm';
+
+  @override
+  String get to => 'İÇİND';
+
+  @override
+  String get your_profile_indicate_id => 'Profiliniz kimliği gösterecek *kullanıcı57676';
+
+  @override
+  String get your_balance_smaller => 'Dengeniz daha küçü';
+
+  @override
+  String get your_access_code => 'Erişim Kodunu';
+
+  @override
+  String get your_promo => 'Promosyon Kodunu';
+
+  @override
+  String get your_mail => 'Mail adresini';
+
+  @override
+  String get your_amount_is => 'Miktarını';
+
+  @override
+  String get your_name => 'Adını';
+
+  @override
+  String get your_goods_and_all_backbone => 'Mallarınız ve tüm omurg';
+
+  @override
+  String get enter_seed => 'Tohum fotoğrafı giri';
+
+  @override
+  String get turn_on_nfc_ny_attach_card => 'NFC \n\'yi açın ve kartı ekleyi';
+
+  @override
+  String get turn_on_nfc => 'NFC\'yi a';
+
+  @override
+  String get to_come_in => 'İçeri ge';
+
+  @override
+  String get enter_seed_phrase => 'Tohum cümlesine göre giri';
+
+  @override
+  String get all => 'Tü';
+
+  @override
+  String get entance_emerald => 'Emerald\'a giri';
+
+  @override
+  String get you_cannot_change_of_empty => 'Boşun adını değiştiremezsi';
+
+  @override
+  String get you_send => 'Göndereceksi';
+
+  @override
+  String get you_get => 'Alacaksını';
+
+  @override
+  String get confirm_logout => 'Kesinlikle dışarı çıkmak istiyor musun';
+
+  @override
+  String get choose_from_gallery => 'Galeri arasından seçim yapı';
+
+  @override
+  String get logout => 'Dışarı çıkma';
+
+  @override
+  String get main => 'E';
+
+  @override
+  String get yes => 'Eve';
+
+  @override
+  String get next => 'Daha öt';
+
+  @override
+  String get account_data => 'Hesap veris';
+
+  @override
+  String get birthday => 'Doğum tarih';
+
+  @override
+  String get it_nesessary_to_get_it => 'Almak gereki';
+
+  @override
+  String get have_to_get => 'Makbuz için gitmiyo';
+
+  @override
+  String get welcome_to_emerald => 'Emerald ayetine hoş geldiniz! \n \n abstat Artırılmış gerçeklik oynayabilir ve borsada hediyeler için değiştirilebilen veya satılabilen kriptocoons ve nft bulabilirsiniz. Hediyelik eşya mağazasında artırılmış gerçeklik. \n \nemorald ayet, bunlar Artırılmış Gerçeklikte AR oyunları, sadakat programları, şehir görevleri ve vay etkileri. Malların ve süpermarketlerin ambalajı';
+
+  @override
+  String get available => 'Mevcu';
+
+  @override
+  String get income => 'Geli';
+
+  @override
+  String get other => 'Diğe';
+
+  @override
+  String get another => 'Bir diğe';
+
+  @override
+  String get more => 'Dah';
+
+  @override
+  String get there_are_still_contacts => 'Hala sözleşme yo';
+
+  @override
+  String get woman => 'Diş';
+
+  @override
+  String get gaming => 'Oyu';
+
+  @override
+  String get game => 'Oyu';
+
+  @override
+  String get from => 'itibare';
+
+  @override
+  String get or => 'vey';
+
+  @override
+  String get name => 'İsi';
+
+  @override
+  String get loose_data => 'Aksi takdirde yaptığınız değişiklikleri kaybedeceksini';
+
+  @override
+  String get inventory => 'Envante';
+
+  @override
+  String get use => 'Kullanma';
+
+  @override
+  String get story => 'Hikay';
+
+  @override
+  String get no_enough => 'Görünüşe göre burada bir şeyler eksik';
+
+  @override
+  String get cards => 'Kar';
+
+  @override
+  String get code_is_wrong => 'Kod yanlı';
+
+  @override
+  String get try_wrong_code_again => 'Yanlış kodu tekrar deneyi';
+
+  @override
+  String get confirmation_code => 'onay kod';
+
+  @override
+  String get quantity => 'Mikta';
+
+  @override
+  String get seto_commission => 'Kare Komisyon';
+
+  @override
+  String get currency_conversion => 'Para birimi dönüştürm';
+
+  @override
+  String get contacts => 'Kontakla';
+
+  @override
+  String get wallet => 'Cüzda';
+
+  @override
+  String get buy => 'Satın alma';
+
+  @override
+  String get buy_coins => 'Jeton satın alı';
+
+  @override
+  String get shop => 'Mağaz';
+
+  @override
+  String get places => 'Ye';
+
+  @override
+  String get my => 'Beni';
+
+  @override
+  String get my_bank_cards => 'Banka Kartları';
+
+  @override
+  String get my_orders => 'Siparişleri';
+
+  @override
+  String get my_pyrchases => 'Satın alımları';
+
+  @override
+  String get my_referals => 'Özetleri';
+
+  @override
+  String get my_seed => 'Benim tohum cümle';
+
+  @override
+  String get man => 'Erke';
+
+  @override
+  String get we_will_automatically_collect_data => 'Verileri otomatik olarak toplayacağız ve gireceği';
+
+  @override
+  String get auth_code_sended => 'Yetkilendirme kodu uygulamaya gönderdi';
+
+  @override
+  String get we_sent_letter_to_restore_account => 'Hesabınızı geri yüklemek için bir mektup gönderdi';
+
+  @override
+  String get on_exchange => 'Borsa üzerind';
+
+  @override
+  String get setting => 'Ayarla';
+
+  @override
+  String get inapproprivate_seed_phrase => 'sadakatsiz tohum cümles';
+
+  @override
+  String get main_incorrectly_introduced => 'Posta yanlış tanıtıld';
+
+  @override
+  String get no => 'HAYI';
+
+  @override
+  String get nickname => 'Nic';
+
+  @override
+  String get card_number => 'Kart numaras';
+
+  @override
+  String get number_not_confirmed => 'Sayı doğrulanmadı, bu nedenle dahili cüzdanın işlevleri çalışmıyo';
+
+  @override
+  String get about_give_back => 'Dönüş hakkınd';
+
+  @override
+  String get about_company => 'Şirket hakkınd';
+
+  @override
+  String get about_app => 'Ek hakkınd';
+
+  @override
+  String get about_sale => 'Eylem hakkınd';
+
+  @override
+  String get general => 'Yaygındı';
+
+  @override
+  String get operation_with_coin => 'Bir madeni para ile operasyo';
+
+  @override
+  String get description => 'Tanı';
+
+  @override
+  String get description_cryptocurrency => 'Binance zinciri ekosistemini destekleyen kripto para biriminin tanımı';
+
+  @override
+  String get main_one => 'An';
+
+  @override
+  String get remain => 'So';
+
+  @override
+  String get cencellation => 'İpta';
+
+  @override
+  String get cancel => 'İptal etme';
+
+  @override
+  String get send => 'Gönderme';
+
+  @override
+  String get send_again => 'Tekrar gönderi';
+
+  @override
+  String get sending_data => 'Veri Gönderm';
+
+  @override
+  String get sending_data_for_analytics => 'Analytics için veri gönderme ve uygulamanın performansını iyileştirm';
+
+  @override
+  String get grades => 'Değerlendirm';
+
+  @override
+  String get erro => 'Hat';
+
+  @override
+  String get translation_error => 'çeviri hatas';
+
+  @override
+  String get error_on_server => 'Sunucuda hata ';
+
+  @override
+  String get affiliate_program => 'Ortaklık Program';
+
+  @override
+  String get personalization => 'Kişiselleştirm';
+
+  @override
+  String get pincodes_not_mutch => 'Pin kodları eşleşmiyo';
+
+  @override
+  String get repeat_access_code => 'Erişim kodunu tekrarlayı';
+
+  @override
+  String get gifts => 'Sunma';
+
+  @override
+  String get support => 'Deste';
+
+  @override
+  String get share => 'Paylaşma';
+
+  @override
+  String get confirm_phone => 'Telefon numarasını onaylayı';
+
+  @override
+  String get confim_your_identity => 'Kimliğinizi onaylayı';
+
+  @override
+  String get confirm => 'Onaylama';
+
+  @override
+  String get confirmation_entrace => 'Uygulamada giriş veya eylemin teyid';
+
+  @override
+  String get number_confirmation => 'Sayı Onay';
+
+  @override
+  String get congratulations_your_place_order => 'Tebrikler! \nvash sipariş verilir !';
+
+  @override
+  String get purchase => 'Satın alma';
+
+  @override
+  String get buying_order => 'Bir Sipariş Satın Alm';
+
+  @override
+  String get sex => 'Zemi';
+
+  @override
+  String get privacy_policy => 'Gizlilik Politikas';
+
+  @override
+  String get get_money_for_invite => 'Davetiyeler için para alın';
+
+  @override
+  String get get => 'Elde etme';
+
+  @override
+  String get get_currency => '%Para Birimi A';
+
+  @override
+  String get replenish => 'Yenileme';
+
+  @override
+  String get try_later => 'daha sonra tekrar deneyi';
+
+  @override
+  String get mail => 'Post';
+
+  @override
+  String get hello => 'Merhab';
+
+  @override
+  String get bind_new_punishment => 'Yeni bir ceza bağlayı';
+
+  @override
+  String get exercise_password => 'Şifreyi kullanı';
+
+  @override
+  String get come_up_with_access_code => 'Bir erişim kodu ile ge';
+
+  @override
+  String get joined => 'Katıld';
+
+  @override
+  String get check_email => 'Postayı kontrol e';
+
+  @override
+  String get sale => 'Satı';
+
+  @override
+  String get continue_ => 'Devam etme';
+
+  @override
+  String get promocode => 'Promosyon kod';
+
+  @override
+  String get promo_codes => 'Promosyon kodlar';
+
+  @override
+  String get profile => 'Profi';
+
+  @override
+  String get public_offer => 'Halka açık tekli';
+
+  @override
+  String get push_notification => 'Push bildirimler';
+
+  @override
+  String get size => 'Boyu';
+
+  @override
+  String get placed => 'Yerleştirilmi';
+
+  @override
+  String get profile_code => 'Profil Düzenlem';
+
+  @override
+  String get reference_code => 'Referans Kod';
+
+  @override
+  String get referrals => 'Yönlendirmele';
+
+  @override
+  String get abstract_programm => 'Soyut progra';
+
+  @override
+  String get referal_code => 'Yönlendirme Kod';
+
+  @override
+  String get russian_ruble => 'Rus Rubles';
+
+  @override
+  String get website => 'İnternet sites';
+
+  @override
+  String get swap => 'Taka';
+
+  @override
+  String get to_make_photo => 'Fotoğraf yapmak içi';
+
+  @override
+  String get sec => 'se';
+
+  @override
+  String get download => 'İndirme';
+
+  @override
+  String get download_quantity => 'İndirilenle';
+
+  @override
+  String get copulated => 'Kopyalanmış';
+
+  @override
+  String get copy => 'Kopy';
+
+  @override
+  String get copy_code => 'Kodu kopyal';
+
+  @override
+  String get hide_name => 'Gerçek adı gizl';
+
+  @override
+  String get events => 'Olayla';
+
+  @override
+  String get creating_order => 'Sipariş Oluşturm';
+
+  @override
+  String get create_wallet => 'Bir cüzdan oluştu';
+
+  @override
+  String get create_your_own_order => 'Kendi Siparişinizi Oluşturu';
+
+  @override
+  String get save => 'Kaydetme';
+
+  @override
+  String get save_chages => 'Değişiklikleri Kaydet';
+
+  @override
+  String get social_net_em => 'Sosyal Ağlar Emeral';
+
+  @override
+  String get validity => 'Geçerlili';
+
+  @override
+  String get standard_partner => 'Standart iş ortağ';
+
+  @override
+  String get sum => 'Öze';
+
+  @override
+  String get current_balance => 'Mevcut bakiy';
+
+  @override
+  String get phone => 'Telefo';
+
+  @override
+  String get product_and_nft => 'Ürünler ve NF';
+
+  @override
+  String get tokens => 'Jeto';
+
+  @override
+  String get you_have => 'Va';
+
+  @override
+  String get you_have_no_puchases_yet => 'Henüz alımınız yok! Doğru :';
+
+  @override
+  String get you_have_no_referrals => 'Henüz yönlendirmeniz yok :';
+
+  @override
+  String get notification => 'Bildirimle';
+
+  @override
+  String get notification_with_confidential_data => 'Gizli verilerle bildirimle';
+
+  @override
+  String get smart_notification => 'Akıllı bildirimle';
+
+  @override
+  String get oops => 'Oops ..';
+
+  @override
+  String get level => 'Seviyes';
+
+  @override
+  String get success_verification => 'Başarılı doğrulam';
+
+  @override
+  String get success => 'Başarıyl';
+
+  @override
+  String get succedssfully_sent => 'Başarıyla gönderild';
+
+  @override
+  String get successfilly_we_return_to_input => 'Başarıyla, girişe geri dönüyoru';
+
+  @override
+  String get surname => 'Soyad';
+
+  @override
+  String get read => 'Okuma';
+
+  @override
+  String get what_is_inside => 'İçerideki ne';
+
+  @override
+  String get develop => 'Bu işlev geliştirilmektedir! Geliştiricilerimiz, tasarımcılarımız ve yöneticilerimiz yeni yongalar yapmak için 7/24 çalışıyor';
+
+  @override
+  String get language => 'Di';
+
+  @override
+  String get apllication_languahe => 'Uygulama dil';
+
+  @override
+  String get ar_card => 'AR kart';
+
+  @override
+  String get ar_scanner => 'AR tarayıcıs';
+
+  @override
+  String get er_qr_games => 'AR, QR, oyunlar vb';
+
+  @override
+  String get bsc_scan => 'BSC taramas';
+
+  @override
+  String get qr_scanner => 'Qr tarayıc';
+}
